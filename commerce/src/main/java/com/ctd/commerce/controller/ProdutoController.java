8ed344d84bfdc037c36cf5575ed75a8452cc7644
Ronilson Alves <ronilsonalves@users.noreply.github.com>
2021-12-09 22:30:58 -0300

    Update ProdutoController.java

--- commerce/src/main/java/com/ctd/commerce/controller/ProdutoController.java
+++ commerce/src/main/java/com/ctd/commerce/controller/ProdutoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://ctdcommercet4g4.vercel.app")
 @RestController
 @RequestMapping("/produto")
 public class ProdutoController {
